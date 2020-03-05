6863309df768a7c003c22d56ad958089e3d2d462
nhrin <57841399+nhrin@users.noreply.github.com>
2020-03-05 10:06:53 +0200

    Update DrawCircle.java

--- src/main/java/DrawFigures/DrawCircle.java
+++ src/main/java/DrawFigures/DrawCircle.java
@@ -7,7 +7,6 @@ public class DrawCircle extends JPanel {
 
     private Color color;
 
-
     public DrawCircle (Color color) {
         JFrame frame = new JFrame("Circle");
         frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
@@ -18,13 +17,9 @@ public class DrawCircle extends JPanel {
         this.color = color;
     }
 
-
-
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-
-
         g2d.setColor(color);
         g2d.fillOval(75, 25, 200, 200);
     }
